610f7e11cac926e3aa175fbfb3c31964f48470f7
Shizuno0525 <8BDI2111@cc.u-tokai.ac.jp>
2021-04-12 17:28:01 +0900

    image resize by pillow

--- image_resize.py
+++ image_resize.py
@@ -2,4 +2,31 @@
 
 #resize practice
 
+import cv2
+import glob
+import os
+from PIL import Image
 
+size=(256,256)
+path='./result'
+
+if not os.path.exists('result'):
+    os.mkdir(path)
+
+files=glob.glob('images/*.png')
+
+
+for file in files:
+    img_name = os.path.basename(file)
+    img=Image.open(file)
+
+    img_resize=img.resize(size)
+    img_resize.save('./result/resized'+img_name)
+
+
+"""    
+    img_name=os.path.basename(file)
+    img=cv2.imread(file)
+    resized_img=cv2.resize(img,dsize=(size))
+    cv2.imread(img_name+"resized.png",resized_img)
+"""
